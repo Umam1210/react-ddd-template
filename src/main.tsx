09d5af78d71b11a3b31480d94140a8c7c51c19f3
umam <khaerul.umam@digitalsekuriti.id>
2024-08-10 13:38:41 +0700

    template react ddd with redux tollkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './core/infrastructure/routing/AppRoutes'
+import { router } from './infrastructure/routing/AppRoutes'
 import { Provider } from 'react-redux'
-import { store } from './core/infrastructure/redux/app/store'
+import { store } from './infrastructure/redux/app/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
